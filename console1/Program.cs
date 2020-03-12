e64fd231d795e435e4969b1eb8f20c1c9d140ec5
komiyasa <komiyasa@microsoft.com>
2020-03-12 23:12:39 +0900

    Parallel.Invoke のテスト。同時に二つの処理が可能。

--- console1/Program.cs
+++ console1/Program.cs
@@ -2,14 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace console1
 {
     class Program
     {
+        static void Task1()
+        {
+            Console.WriteLine("Task 1 Starting");
+            Thread.Sleep(2000);
+            Console.WriteLine("Task 1 Finish");
+        }
+
+        static void Task2()
+        {
+            Console.WriteLine("Task 2 Starting");
+            Thread.Sleep(1000);
+            Console.WriteLine("Task 2 Finish");
+        }
+
         static void Main(string[] args)
         {
+            Parallel.Invoke(() => Task1(), () => Task2());
+            Console.WriteLine("Finish All Task. Press a any key to end.");
+            Console.ReadKey();
         }
     }
 }
